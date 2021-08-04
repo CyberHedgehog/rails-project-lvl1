66665773a742af67fb91ad80d8120d0064c49582
dmitriy <d.dobrenkiy@hotmail.com>
2021-08-04 21:24:54 +0300

    fix: style

--- lib/hexlet_code/form_for.rb
+++ lib/hexlet_code/form_for.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module HexletCode
-  def self.form_for(entity, url = '#')
+  def self.form_for(_entity, url = '#')
     self::Tag.build('form', { action: url, method: 'post' }) { '' }
   end
 end
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -3,11 +3,9 @@
 module HexletCode
   module Tag
     def self.build(name, params = {})
-      params_string = params
-        .each_with_object([]) { |(key, value), list| list << " #{key}=\"#{value}\"" }
-        .join('')
+      params_string = params.each_with_object([]) { |(key, value), list| list << " #{key}=\"#{value}\"" }.join
       tag = "<#{name}#{params_string}>"
-      (block_given?) ? "#{tag}#{yield}</#{name}>" : tag
+      block_given? ? "#{tag}#{yield}</#{name}>" : tag
     end
   end
 end
